cd48f95b68e270b528051031e26483eac5a03036
ryandagg <ryan.dagg@gmail.com>
2014-07-01 11:14:51 -0600

    non-Bonus sections finished, but a bit ugly still

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).on('ready', function() {
-  
+	var outerShell = $('<div class="outer-shell"></div>');
+ 	var innerShell = $('<div class="inner-shell"></div>');
+ 	var clockScreen = $('<div class="clock-screen"></div>');
+ 	var clockText = $('<div class="clock-text">12:34</div>')
+ 	var pmText = $("<div class='pm-text'>PM</div>");
+ 	var autoText = $("<div class='auto-text'>AUTO</div>");
+ 	var pmDot = $("<div class='pm-dot'>*</div>");
+ 	var autoDot = $("<div class='auto-dot'>*</div>");
+ 	var amFreqText = $("<div class='am-freq-text'>AM  60  70  90  110  140  170     KHz</div>");
+ 	var pmFreqText = $("<div class='fm-freq-text'>FM  83  96  102  106  108     MHz</div>");
+
+
+
+ 	clockScreen.append(pmDot);
+ 	clockScreen.append(clockText);
+ 	clockScreen.append(autoDot);
+
+ 	innerShell.append(pmText);
+	innerShell.append(clockScreen);
+	innerShell.append(amFreqText)
+	innerShell.append(pmFreqText)
+	innerShell.append(autoText)
+
+ 	outerShell.append(innerShell);
+ 	
+ 	$('.container').append(outerShell);
 });
